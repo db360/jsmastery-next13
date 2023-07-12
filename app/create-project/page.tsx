b3c45fde0827a07a727d177f72deab890cb6c7f5
David Martinez <da.b@hotmail.es>
2023-07-12 11:53:03 +0200

    formField, projectForm, page redirect

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -1,7 +1,7 @@
+import { redirect } from "next/navigation";
 import Modal from "@/components/Modal"
 import ProjectForm from "@/components/ProjectForm"
 import { getCurrentUser } from "@/lib/session"
-import { redirect } from "next/navigation";
 
 const CreateProject = async() => {
     const session = await getCurrentUser();
--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,6 +1,10 @@
 'use client';
 
 import { SessionInterface } from "@/common.types"
+import Image from "next/image";
+import { ChangeEvent } from "react";
+import FormField from "./FormField";
+import { categoryFilters } from "@/constants";
 
 type Props = {
     type: string,
@@ -13,12 +17,92 @@ const ProjectForm = ({ type, session} : Props) => {
 
     };
 
+    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+
+    }
+
+    const handleStateChange = (fieldName : string, value: string) => {
+
+    }
+
+    const form = {
+      image: '',
+      title: '',
+
+    };
 
   return (
     <form
         onSubmit={handleFormSubmit}
         className="flexStart form"
     >
+      <div className="flexStart form_image-container">
+        <label htmlFor="poster" className="flexCenter form_image-label">
+          {!form.image && 'Choose a poster for your project'}
+        </label>
+        <input
+          type="file"
+          id="image"
+          accept="image/*" // Tod-os los tipos de imágenes
+          required={type === 'create'} // si el tipo es create
+          onChange={handleChangeImage}
+          className="form_image-input"
+        />
+        {form.image && (
+          <Image
+            src={form?.image}
+            className="sm:p-10 object-contain z-20"
+            alt="Project Poster"
+            fill
+          />
+        )}
+      </div>
+
+      <FormField
+        title="Title"
+        state={form.title}
+        placeholder="Flexibble"
+        setState={(value) => handleStateChange('title', value)}
+      />
+      <FormField
+        title="Description"
+        state={form.description}
+        placeholder="Showcase and discover remarkable developer projects"
+        setState={(value) => handleStateChange('description', value)}
+      />
+      <FormField
+        type="url"
+        title="Website URL"
+        state={form.liveSiteUrl}
+        placeholder="https://dabmartinez.com"
+        setState={(value) => handleStateChange('liveSiteUrl', value)}
+      />
+      <FormField
+        type="url"
+        title="Github URL"
+        state={form.githubUrl}
+        placeholder="https://github.com/db360"
+        setState={(value) => handleStateChange('githubUrl', value)}
+      />
+      <FormField
+        title="Title"
+        state={form.title}
+        placeholder="Flexibble"
+        setState={(value) => handleStateChange('title', value)}
+      />
+
+      {/* CustomInput Category...*/}
+      <CustomMenu
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        
+
+      />
+
+      <div className="flexStart w-full">
+        <button>Create</button>
+      </div>
 
     </form>
   )
