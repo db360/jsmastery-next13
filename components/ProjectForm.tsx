3695c7be7aafc91249c3da5e65358814e9123db8
db360 <da.b@hotmail.es>
2023-07-12 14:42:41 +0200

    handleChangeImage, States from Form

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,50 +1,70 @@
-'use client';
+"use client";
 
-import { SessionInterface } from "@/common.types"
+import { SessionInterface } from "@/common.types";
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import FormField from "./FormField";
 import { categoryFilters } from "@/constants";
+import CustomMenu from "./CustomMenu";
 
 type Props = {
-    type: string,
-    session: SessionInterface
-}
+  type: string;
+  session: SessionInterface;
+};
 
-const ProjectForm = ({ type, session} : Props) => {
+const ProjectForm = ({ type, session }: Props) => {
+  const handleFormSubmit = (e: React.FormEvent) => {};
 
-    const handleFormSubmit = (e: React.FormEvent) => {
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
 
-    };
+    const file = e.target.files?.[0];
 
-    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    if(!file) return;
 
+    if(!file.type.includes('image')) {
+      return alert('Please upload an image file')
     }
 
-    const handleStateChange = (fieldName : string, value: string) => {
+    const reader = new FileReader();
 
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+
+      handleStateChange('image', result);
     }
 
-    const form = {
-      image: '',
-      title: '',
+  };
 
-    };
+  const handleStateChange = (fieldName: string, value: string) => {
+    setForm((prevState) => ({ ...prevState, [fieldName]: value }));
+  };
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    image: "",
+    liveSiteUrl: "",
+    githubUrl: "",
+    category: "",
+  });
 
   return (
-    <form
-        onSubmit={handleFormSubmit}
-        className="flexStart form"
-    >
+    <form onSubmit={handleFormSubmit} className="flexStart form">
       <div className="flexStart form_image-container">
         <label htmlFor="poster" className="flexCenter form_image-label">
-          {!form.image && 'Choose a poster for your project'}
+          {!form.image && "Choose a poster for your project"}
         </label>
         <input
+          title="Form Image Input"
           type="file"
           id="image"
           accept="image/*" // Tod-os los tipos de imágenes
-          required={type === 'create'} // si el tipo es create
+          required={type === "create"} // si el tipo es create
           onChange={handleChangeImage}
           className="form_image-input"
         />
@@ -62,33 +82,33 @@ const ProjectForm = ({ type, session} : Props) => {
         title="Title"
         state={form.title}
         placeholder="Flexibble"
-        setState={(value) => handleStateChange('title', value)}
+        setState={(value) => handleStateChange("title", value)}
       />
       <FormField
         title="Description"
         state={form.description}
         placeholder="Showcase and discover remarkable developer projects"
-        setState={(value) => handleStateChange('description', value)}
+        setState={(value) => handleStateChange("description", value)}
       />
       <FormField
         type="url"
         title="Website URL"
         state={form.liveSiteUrl}
         placeholder="https://dabmartinez.com"
-        setState={(value) => handleStateChange('liveSiteUrl', value)}
+        setState={(value) => handleStateChange("liveSiteUrl", value)}
       />
       <FormField
         type="url"
         title="Github URL"
         state={form.githubUrl}
         placeholder="https://github.com/db360"
-        setState={(value) => handleStateChange('githubUrl', value)}
+        setState={(value) => handleStateChange("githubUrl", value)}
       />
       <FormField
         title="Title"
         state={form.title}
         placeholder="Flexibble"
-        setState={(value) => handleStateChange('title', value)}
+        setState={(value) => handleStateChange("title", value)}
       />
 
       {/* CustomInput Category...*/}
@@ -96,16 +116,14 @@ const ProjectForm = ({ type, session} : Props) => {
         title="Category"
         state={form.category}
         filters={categoryFilters}
-        
-
+        setState={(value) => handleStateChange("category", value)}
       />
 
       <div className="flexStart w-full">
         <button>Create</button>
       </div>
-
     </form>
-  )
-}
+  );
+};
 
-export default ProjectForm
+export default ProjectForm;
